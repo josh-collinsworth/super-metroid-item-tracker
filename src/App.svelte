adc11bf155c3a6a43e7c614fd392e7431d5782a3
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-01-25 20:34:13 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,338 @@
+<script>
+import { afterUpdate } from 'svelte'
+import { fly, fade } from 'svelte/transition'
+import { quintIn, quintOut, quintInOut } from 'svelte/easing'
+import { defaultList } from './defaultList.js'
+
+export let initializeList = () => {
+	if (!localStorage.getItem('superMetroidItemChecklist')) {
+		localStorage.setItem('superMetroidItemChecklist', JSON.stringify(defaultList))
+	}
+	return JSON.parse(localStorage.getItem('superMetroidItemChecklist'))
+}
+
+export let initializeOptions = () => {
+	if (!localStorage.getItem('superMetroidItemChecklistOptions')) {
+		localStorage.setItem('superMetroidItemChecklistOptions', JSON.stringify({ sorting: 'item', hideChecked: false }))
+	}
+	return JSON.parse(localStorage.getItem('superMetroidItemChecklistOptions'))
+}
+
+export let items = initializeList();
+export let options = initializeOptions();
+export let isMenuOpen = false;
+export let menubutton;
+
+
+afterUpdate(() => {
+	localStorage.setItem('superMetroidItemChecklist', JSON.stringify(items))
+	localStorage.setItem('superMetroidItemChecklistOptions', JSON.stringify(options))
+	console.log(options.hideChecked)
+});
+
+const itemName = (item) => {
+	return item.type === "Powerup" ? item.subtype : item.type
+}
+
+const resetList = () => {
+	const confirmation = confirm(`Are you sure you want to completely reset the list and all items to zero?`);
+	if (confirmation) items = [...defaultList];
+}
+
+const viewByItem = () => {
+	return options.sorting === "item" ? null : 'hidden';
+}
+
+const toggleMenu = () => {
+	isMenuOpen = !isMenuOpen;
+	menubutton.setAttribute('aria-pressed', !isMenuOpen);
+}
+
+
+$: orderedItems = items.sort((a, b) => a.order - b.order);
+
+$: percentage = items.filter(item => item.checked).length
+
+$: missiles = items.filter(item => item.subtype === "Missile")
+$: eTanks = items.filter(item => item.subtype === "Energy Tank")
+$: reserveTanks = items.filter(item => item.subtype === "Reserve Tank")
+$: superMissiles = items.filter(item => item.subtype === "Super Missile")
+$: powerBombs = items.filter(item => item.subtype === "Power Bomb")
+$: powerUps = items.filter(item => item.subtype === "Upgrade")
+
+$: collectedMissiles = missiles.filter(item => item.checked)
+$: collectedETanks = eTanks.filter(item => item.checked)
+$: remainingETanks = eTanks.filter(item => !item.checked)
+$: collectedReserveTanks = reserveTanks.filter(item => item.checked)
+$: collectedSuperMissiles = superMissiles.filter(item => item.checked)
+$: collectedPowerBombs = powerBombs.filter(item => item.checked)
+$: collectedPowerUps = powerUps.filter(item => item.checked)
+
+</script>
+
+<main>
+	<header id="header" class="category">
+
+	<h1>Super Metroid Item Checklist</h1>
+		<h2>Total: {percentage}%</h2>
+
+		<button bind:this={menubutton} on:click={toggleMenu} aria-pressed="false" role="button">Options</button>
+	</header>
+
+	{#if isMenuOpen}
+		<aside id="menu" transition:fly="{{x: -400, duration: 400}}">
+			<form>
+				<button id="close-menu-btn" on:click|preventDefault="{toggleMenu}" role="button" aria-label="close menu">❌</button>
+				<fieldset id="viewSwitch">
+					<legend>View by:</legend>
+					<div class="form-pair">
+						<input bind:group={options.sorting} value={'item'} type="radio" id="byType" />
+						<label for="byType">Item type</label>
+					</div>
+					<div class="form-pair">
+						<input bind:group={options.sorting} value={'order'} type="radio" id="byOrder" />
+						<label for="byOrder">Speed run order</label>
+					</div>
+				</fieldset>
+
+				<div class="form-pair">
+					<input bind:checked={options.hideChecked} type="checkbox" id="hideChecked" />
+					<label for="hideChecked">Hide checked items</label>
+				</div>
+
+				<button on:click|preventDefault={resetList} role="button">Reset list</button>
+			</form>
+		</aside>
+	{/if}
+
+
+
+	{#if options.sorting === "item"}
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="eTanks" class="category">
+			<h3>Energy Tanks: {collectedETanks.length}/{eTanks.length}</h3>
+			<ul>
+				{#each eTanks as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="etank-{index + 1}" bind:checked={item.checked}>
+						<label for="etank-{index + 1}">{item.location || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="missiles" class="category">
+			<h3>Missiles: {collectedMissiles.length}/{missiles.length}</h3>
+			<ul>
+				{#each missiles as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="missile-{index + 1}" bind:checked={item.checked}>
+						<label for="missile-{index + 1}">{item.location || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="reserveTanks" class="category">
+			<h3>Reserve Tanks: {collectedReserveTanks.length}/{reserveTanks.length}</h3>
+			<ul>
+				{#each reserveTanks as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="reserve_tank-{index + 1}" bind:checked={item.checked}>
+						<label for="reserve_tank-{index + 1}">{item.location || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="superMissiles" class="category">
+			<h3>Super Missiles: {collectedSuperMissiles.length}/{superMissiles.length}</h3>
+			<ul>
+				{#each superMissiles as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="super_missile-{index + 1}" bind:checked={item.checked}>
+						<label for="super_missile-{index + 1}">{item.location || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="powerBombs" class="category">
+			<h3>Power Bombs: {collectedPowerBombs.length}/{powerBombs.length}</h3>
+			<ul>
+				{#each powerBombs as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="power_bomb-{index + 1}" bind:checked={item.checked}>
+						<label for="power_bomb-{index + 1}">{item.location || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+		<div in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" id="powerUps" class="category">
+			<h3>Power-Ups: {collectedPowerUps.length}/{powerUps.length}</h3>
+			<ul>
+				{#each powerUps as item, index}
+				{#if !options.hideChecked || !item.checked }
+					<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+						<input type="checkbox" id="upgrade-{index + 1}" bind:checked={item.checked}>
+						<label for="upgrade-{index + 1}">{item.type || ''}</label>
+					</li>
+					{/if}
+				{/each}
+			</ul>
+		</div>
+
+	{:else}
+
+		<ol in:fly="{{ y: 20, duration: 350, easing: quintOut, delay: 350 }}" out:fly="{{ y: 20, duration: 350, easing: quintIn }}" class="category">
+			<h3>All items</h3>
+			{#each orderedItems as item}
+				{#if !options.hideChecked || !item.checked }
+				<li transition:fly="{{y:10, duration: 350, easing: quintInOut}}">
+					<input type="checkbox" id="item-{item.order}" bind:checked={item.checked}>
+					<label for="item-{item.order}">
+						<strong>{itemName(item)}</strong>
+						&ensp;|&ensp;
+						{item.location || ''}
+					</label>
+				</li>
+				{/if}
+			{/each}
+		</ol>
+
+	{/if}
+
+</main>
+
+<footer>
+	<p>This site ©2020 Josh Collinsworth. Built with <a href="https://svelte.dev/" target="_blank">Svelte</a> and hosted on <a href="https://netlify.com/" target="_blank">Netlify</a>.</p>
+	<p>Super Metroid ©1994 Nintendo.</p>
+</footer>
+
+<style>
+	main {
+		padding: 2em 2em 8em;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+		grid-gap: 1rem;
+	}
+
+	h1 {
+		grid-column: span 2;
+	}
+
+	header {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		justify-items: start;
+		align-items: center;
+		grid-column: 1 / -1;
+	}
+
+	header h1,
+	header h2 {
+		grid-column: 1 / 4;
+	}
+
+	header button {
+		grid-row: 1 / 3;
+		display: block;
+		justify-self: end;
+	}
+
+	header button {
+		grid-column: 5 / 6;
+	}
+
+	li {
+		margin: 0 0 .25em;
+	}
+
+	#itemTypeSection, #orderSection {
+		display: inherit;
+		grid-template-columns: inherit;
+		grid-gap: inherit;
+	}
+
+	.category {
+		border: 2px solid;
+		border-radius: .5em;
+		padding: 1rem;
+		background: rgba(1, 0, 27, .6);
+	}
+
+	.category label {
+		font-weight: normal;
+	}
+
+	#etank-hud {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		max-width: 5rem;
+	}
+
+	label {
+		font-weight: bold;
+	}
+
+	ol.category {
+		padding-left: 2rem;
+		grid-column: 1 / -1;
+	}
+
+	#menu {
+		position: fixed;
+		top: 0;
+		left: 0;
+		min-height: 100vh;
+		padding: 2rem;
+		width: 400px;
+		background: rgba(0,0,0,.9);
+		z-index: 10;
+	}
+
+	.hidden {
+		display: none;
+	}
+
+	#close-menu-btn {
+		position: absolute;
+		right: 1rem;
+		top: 1rem;
+		background: transparent;
+		border: none;
+	}
+
+	#menu form > * + * {
+		margin-top: 2rem;
+	}
+
+	footer {
+		padding: 2em;
+		text-align: center;
+	}
+
+	@media (min-width: 70rem) {
+		ol.category {
+			column-count: 2;
+			column-gap: 4rem;
+		}
+	}
+
+	@media (min-width: 110rem) {
+		ol.category {
+			column-count: 3;
+		}
+	}
+</style>
---
+++ src/defaultList.js
@@ -0,0 +1,605 @@
+module.exports = {
+	defaultList: [
+		{
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Awakening",
+			order: 2
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "x2 To the right of Early Supers",
+			order: 6
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "x2 To the right of Early Supers",
+			order: 7
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Below the bridge",
+			order: 9
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hi-Jump Boots",
+			order: 16
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "In center Cathedral lava pit",
+			order: 17
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden in ceiling right before Speed Booster door",
+			order: 18
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Wave Beam",
+			order: 20
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Grappling Beam",
+			order: 25
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Lava tide",
+			order: 26
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Next to green gate",
+			order: 27
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden behind KPDR any% Power Bomb",
+			order: 28
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Moat",
+			order: 31
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden to the left of the main Wrecked Ship shaft",
+			order: 32
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Right of attic",
+			order: 36
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden in highest point of the mountain to the left",
+			order: 37
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden in ground maze to the left",
+			order: 38
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Gravity Suit",
+			order: 39
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Bottom left of ocean",
+			order: 42
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "x2 Crumble shaft after Gauntlet",
+			order: 45
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "x2 Crumble shaft after Gauntlet",
+			order: 46
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Behind the Grapple blocks",
+			order: 50
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Charge Beam",
+			order: 54
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Green Hill pipe",
+			order: 56
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Main Street",
+			order: 57
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Mama Turtle",
+			order: 58
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden in right side of Maridian beach",
+			order: 61
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Also hidden in sand pit below a Choot",
+			order: 63
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Draygon",
+			order: 64
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "East Sand Hole, drop down through the right sand pit",
+			order: 67
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Aqueduct, top right pipes of Aqueduct",
+			order: 71
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "West Sand Hole, also drop down through the left sand pit",
+			order: 73
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Left over in Kraid's Lair, accessed with a Power Bomb",
+			order: 75
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Lower route after Ice Beam - hidden directly left in the wall of Geemer shaft after entering room",
+			order: 77
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Golden Torizo - watch out for that floor too",
+			order: 78
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Mickey Mouse",
+			order: 81
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "In front of pipe maze",
+			order: 85
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden left of 'Three Muskateers'",
+			order: 87
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Top left green door in Bubble Mountain",
+			order: 88
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Hidden inside the sphere closest to the previous Reserve Tank",
+			order: 90
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Bubble Mountain, bottom right corner of Bubble Mountain",
+			order: 91
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Ceiling right past Construction Zone, to the right",
+			order: 94
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "To the right of the right of the construction zone, up the shaft and past the invisible bridge with the falling boulders",
+			order: 96
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "To the right of the right of the construction zone, also up the shaft and past the invisible bridge with the falling boulders",
+			order: 97
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Below old Mother Brain tank",
+			order: 98
+		}, {
+			type: "Powerup",
+			subtype: "Missile",
+			checked: false,
+			location: "Final, first left Parlor door after climb",
+			order: 100
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Early",
+			order: 5
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Wrecked Ship East, past the robots",
+			order: 33
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Wrecked Ship West",
+			order: 34
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Etecoon, green door to the left",
+			order: 48
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Spore Spawn",
+			order: 53
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Main Street, Crab Supers",
+			order: 60
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Hidden in sand pit below a Choot",
+			order: 62
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Aqueduct, top right pipes of aqueduct",
+			order: 72
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Golden Torizo",
+			order: 79
+		}, {
+			type: "Powerup",
+			subtype: "Super Missile",
+			checked: false,
+			location: "Crateria, hidden right of climb - bomb first platform on right side and shinespark up shaft",
+			order: 99
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "First tank, a few rooms left of the landing site in Crateria",
+			order: 4
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "The gray door just inside Kraid's lair, unlocked after defeating Kraid",
+			order: 13
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Hi-Jump Boots room",
+			order: 14
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Far right of Crocomire's room",
+			order: 22
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Wrecked ship, waterlogged spike room",
+			order: 35
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "\"Gauntlet\" (behind the wall above Samus's ship on the left) ",
+			order: 44
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Brinstar, through the bottom floor of the first corridor from Crateria",
+			order: 47
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Brinstar, room with hoppers above the Spore Spawn exit",
+			order: 52
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Behind and below the Charge Beam",
+			order: 55
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Maridia, in the \"turtle\" room up/right from the glass tube",
+			order: 58
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Just after Botwoon (Maridia mini-boss)",
+			order: 66
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "After the fight with Ridley",
+			order: 83
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Far right of lower Norfair in the \"Fireflea\" room, hidden behind a wall and in the lower right",
+			order: 84
+		}, {
+			type: "Powerup",
+			subtype: "Energy Tank",
+			checked: false,
+			location: "Ceiling of old Brinstar (same location as original Metroid)",
+			order: 94
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Missile door just to the left of Crocomire",
+			order: 23
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Brinstar, at the bottom of the shaft below the far right Crateria elevator",
+			order: 29
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Brinstar, first door from the far right Crateria elevator",
+			order: 30
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "The door in the far upper-right of the landing site",
+			order: 43
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Brinstar; the shaft with the Etecoons (wall jump)",
+			order: 49
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Hidden behind/below the Grapple blocks",
+			order: 51
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "East Sand Hole, also drop down through the right sand pit",
+			order: 68
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "of shame",
+			order: 82
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Below the door after pipe maze",
+			order: 86
+		}, {
+			type: "Powerup",
+			subtype: "Power Bomb",
+			checked: false,
+			location: "Retro Brinstar, above Green Hill",
+			order: 93
+		}, {
+			type: "Powerup",
+			subtype: "Reserve Tank",
+			checked: false,
+			location: "In the hands of the Chozo",
+			order: 8
+		}, {
+			type: "Powerup",
+			subtype: "Reserve Tank",
+			checked: false,
+			location: "Gravity Suit",
+			order: 40
+		}, {
+			type: "Powerup",
+			subtype: "Reserve Tank",
+			checked: false,
+			location: "West Sand Hole, drop down through the left sand pit this time",
+			order: 73
+		}, {
+			type: "Powerup",
+			subtype: "Reserve Tank",
+			checked: false,
+			location: "Through a hidden path to the left of the top left door in bubble mountain",
+			order: 89
+		}, {
+			type: "Bombs",
+			subtype: "Upgrade",
+			checked: false,
+			location: "Chorizo room",
+			order: 3
+		}, {
+			type: "Charge Beam",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 9
+		}, {
+			type: "Spazer",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 10
+		}, {
+			type: "Varia Suit",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 11
+		}, {
+			type: "Powerup",
+			subtype: "Hi-Jump Boots",
+			checked: false,
+			location: undefined,
+			order: 15
+		}, {
+			type: "Speed Booster",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 19
+		}, {
+			type: "Wave Beam",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 21
+		}, {
+			type: "Grappling Beam",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 24
+		}, {
+			type: "Gravity Suit",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 41
+		}, {
+			type: "Morphing Ball",
+			subtype: "Upgrade",
+			checked: false,
+			location: "Old Brinstar",
+			order: 1
+		}, {
+			type: "Space Jump",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 65
+		}, {
+			type: "Spring Ball",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 69
+		}, {
+			type: "Plasma Beam",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 70
+		}, {
+			type: "Ice Beam",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 76
+		}, {
+			type: "Screw Attack",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 80
+		}, {
+			type: "X-Ray Scope",
+			subtype: "Upgrade",
+			checked: false,
+			location: undefined,
+			order: 92
+		}
+	]
+}

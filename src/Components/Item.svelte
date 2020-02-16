ab3cc73c2bf55a54989bde3ef4b0a9e7662fe6e1
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2020-02-15 22:02:29 -0600

    New Item component and overhauled default List

---
+++ src/Components/Item.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let item, sluggify, options
+</script>
+
+
+<li class:collapsed="{options.hideChecked && item.checked}">
+	<input type="checkbox" id="item-{item.order}" bind:checked={item.checked}>
+	<label for="item-{item.order}">
+		{#if options.sorting != 'item'}
+			<img class="icon" src={`../../icons/${sluggify(item.name)}.png`} alt="">
+		{/if}
+		<strong>{item.name}</strong>
+		{#if item.location}
+			<div class="location" class:collapsed="{options.hideLocations}">
+				{item.location}
+			</div>
+		{/if}
+	</label>
+</li>
